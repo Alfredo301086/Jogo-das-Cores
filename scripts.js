ab43eabf31bd64a6aaaea8df0d743912290e1dd9
Alfredo Junior Felinto Moreira <alfredojfm86@gmail.com>
2023-02-22 20:20:10 -0300

    jogo

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function colorido() {
+    titulo = document.getElementById('cabecalho')
+    titulo.innerText = 'TESTE'
+    a = Math.floor(Math.random() * 255)+1
+    console.log(a)
+}
+function comecar() {
+
+}
+
+function parar(){
+
+}
+
+function corAleatoria() {
+    rgb(Math.floor(Math.random() * 255)+1, Math.floor(Math.random() * 255)+1, Math.floor(Math.random() * 255)+1)
+}
